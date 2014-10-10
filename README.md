9acac0ebdaa1320b6e29fa3fb83af3bee9228688
vitogav <vito.gav@gmail.com>
2014-10-10 15:00:07 -0700

    This is the initial push of the project.
    You should be able to clone this and link it with the tess two library
    and it should run just showing a google map. If it fails and log cat
    gives an error from trying to load TessBaseApi, you have not built the
    tess two library correctly.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ ParkingTracker
 ==============
 
 An android application for tracking parking usage within a neighborhood.
+
+
+To build you must use the https://github.com/rmtheis/tess-two TessTwo library. Follow their instructions but make sure you have Android NDK r9d and not r10b. Making sure ndk-build, android, and ant are all on the PATH enviroment variable will help get everything installed. After you build TessTwo, set it as a library in eclipse and then link it to your ParkingTracker app as a library.
+
+You will be required to have the Android-14 sdk as well as the google play services library.
---
+++ src/com/southwaterfront/parkingtracker/Main.java
@@ -0,0 +1,46 @@
+package com.southwaterfront.parkingtracker;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		TessBaseAPI baseApi = new TessBaseAPI();
+		String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Tess/";
+		try {
+			baseApi.init(path, "eng");
+		} catch (Exception e) {
+			Log.e("Tess", e.getMessage());
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
