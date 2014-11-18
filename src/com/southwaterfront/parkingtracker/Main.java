3e832a9e9b8209a3fc1fa6b1865f9ce478adba87
Joel Uyesugi <j.uyesugi@wsu.edu>
2014-11-18 08:12:55 -0800

    Added the ability to take and store photos.
    Photos are currently stored in /storage/emulated/0/Pictures
    
    Also updated the ic_launcher.png

--- src/com/southwaterfront/parkingtracker/Main.java
+++ src/com/southwaterfront/parkingtracker/Main.java
@@ -1,9 +1,23 @@
 package com.southwaterfront.parkingtracker;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.southwaterfront.parkingtracker.AssetManager.AssetManager;
 
@@ -11,6 +25,91 @@ public class Main extends Activity {
 
 	private static final String LOG_TAG = "Main";
 
+	// -------------------------------------------------------------------------------------------------
+	// Temp placement of code
+	// -------------------------------------------------------------------------------------------------
+	
+	static final int REQUEST_TAKE_PHOTO = 2;
+	String mCurrentPhotoPath;
+	
+	private File createImageFile() throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "IMG_" + timeStamp + "_";
+	    File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+
+	    // Save a file: path for use with ACTION_VIEW intents
+	    //mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    mCurrentPhotoPath = image.getAbsolutePath();
+	    return image;
+	}
+	
+	private void dispatchTakePictureIntent2(){
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    // Ensure that there's a camera activity to handle the intent
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        // Create the File where the photo should go
+	        File photoFile = null;
+	        try {
+	            photoFile = createImageFile();
+	        } catch (IOException ex) {
+	            // Error occurred while creating the File
+	        }
+	        // Continue only if the File was successfully created
+	        if (photoFile != null) {
+	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	                    Uri.fromFile(photoFile));
+	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+	        }
+	    }
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+	        //galleryAddPic();
+	        setPic();
+	    }
+	}
+	
+	private void setPic() {
+		final ImageView imageView = (ImageView)  findViewById(R.id.imageView1);
+		
+	    // Get the dimensions of the View
+	    int targetW = imageView.getWidth();
+	    int targetH = imageView.getHeight();
+
+	    // Get the dimensions of the bitmap
+	    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+	    bmOptions.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+	    int photoW = bmOptions.outWidth;
+	    int photoH = bmOptions.outHeight;
+
+	    
+	    // Determine how much to scale down the image
+	    int scaleFactor;
+	    if (targetW == 0 || targetH == 0){
+	    	scaleFactor = 1;
+	    } else {
+		    scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+	    }
+
+	    // Decode the image file into a Bitmap sized to fill the View
+	    bmOptions.inJustDecodeBounds = false;
+	    bmOptions.inSampleSize = scaleFactor;
+	    bmOptions.inPurgeable = true;
+
+	    Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+	    imageView.setImageBitmap(bitmap);
+	}
+	// -------------------------------------------------------------------------------------------------
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,6 +119,15 @@ public class Main extends Activity {
 		AssetManager assetManager = AssetManager.getInstance();
 
 		assetManager.assetSanityCheck();
+		
+		// Temp Button init location
+		final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	dispatchTakePictureIntent2(); 	
+            }
+        });
 		
 	}
 
